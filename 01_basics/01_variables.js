ecb753e54720fe9005eab9419b8168b0c45b4a20
manthan20021 <126886816+manthan20021@users.noreply.github.com>
2023-03-09 04:11:21 +0000

    account chenges done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1212
+let accountEmail = "manthan@gmail.com"
+var accountPassword = "200924"
+accountCity = "amaravati"
+
+
+//accountId = 23 this is not allowed
+
+accountEmail = "mRh@gmail.com"
+accountPassword = "02020292"
+accountCity = "pune"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
