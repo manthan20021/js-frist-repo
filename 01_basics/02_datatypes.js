aa558089f07fd14059624798f5cb2952f02c5526
manthan20021 <126886816+manthan20021@users.noreply.github.com>
2023-03-09 02:30:03 -0800

    detatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) //we are using nodejs,not browser
+
+console.log( 3
+    + 3) // code readability should be high
+
+    console.log("manthan")
+
+
+    let name = "manthan"
+    let age = 19
+    let isLoggedIn = false
