e8c47879cf4d26edde4349907f7fbb75fb1ca6d7
manthan20021 <126886816+manthan20021@users.noreply.github.com>
2023-03-11 13:53:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = null
+
+console.log(typeof score)
+console.log(score)
+
+let valueInNumber = Number (score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//notes
+
+//"33" => 33 (this is a convertabel)
+//"33abc" => NaN but it's data type is number,this is confusing (this is not converteble)
+//true => 1 
+//flass => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedin = Boolean (isLoggedIn)
+console.log(booleanIsLoggedin)
+
+
+
+//boolean valus nots
+
+//1 => true 0 => flass
+//"" => flass
+//"manthan" => true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
