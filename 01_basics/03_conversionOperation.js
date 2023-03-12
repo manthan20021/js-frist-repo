7f23663d0699769b7977ac607c9b7d17ce835467
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2023-03-12 12:59:33 +0000

    Operations den

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = null
 
-console.log(typeof score)
-console.log(score)
+//console.log(typeof score)
+//console.log(score)
 
 let valueInNumber = Number (score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //notes
 
@@ -18,7 +18,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedin = Boolean (isLoggedIn)
-console.log(booleanIsLoggedin)
+//console.log(booleanIsLoggedin)
 
 
 
@@ -30,4 +30,44 @@ console.log(booleanIsLoggedin)
 
 let someNumber = 22
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+
+//************************************opretion**************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+ console.log( (3 + 4) * 5 % 3);
+
+ console.log(+true);
+ console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
