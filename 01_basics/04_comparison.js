e20e95c7521d73404a27c35c3e16e5e4b4ef054b
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2023-03-17 05:21:31 +0000

    comparison is done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// also avaid this type of coparison and convarsion
+
+console.log(undefined == 0); 
+console.log(undefined > 0);
+console.log(undefined < 0); 
+
+//comparison and equality chack this both are defrant thainks
+
+
+
+// === 
+
+console.log("2" === 2);
+
+
+// this is strick chack and this is not only chick your valu but also chack your datatype
