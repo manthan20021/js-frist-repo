208ccf22f170144b9bb2757b5b9adfff5c9279b4
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2023-03-28 06:13:49 +0000

    cretion in code

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,7 +1,7 @@
 const name = "manthan"
 const repoCount = 50
 
-//console.log(name + repoCount) // note => this is a old methode 
+console.log(name + repoCount) // note => this is a old methode 
 
 console.log('Hello my name is ${name} and my repo count is ${repoCount}');  // note => allwes go with this bicos of this is a moer redebel and morden method.
 
@@ -33,7 +33,7 @@ console.log(anotherString); // nigetiv subsring.
 const newStringOne = "  manthanh  "
 
 console.log(newStringOne);
-console.log(newStringOne.trim()); //trim is the ueis for cut/trim the extra spses, ther is a 2 part of trim, trim-start and trim-eand. 
+console.log(newStringOne.trim()); //the use of a trim is cut/trim the extra spses, ther is a 2 part of trim, trim-start and trim-eand. 
 
 
 const url = "https://manthan.com/manthan%30harale"
